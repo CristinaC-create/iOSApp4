6a424b41ae0ed61eeb14c04844ca0bf3c271ffc5
Cristina <Cristina.Cabral@triosstudent.com>
2025-02-07 19:09:35 -0500

    Initial Commit

---
+++ iOSApp4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOSApp4
+//
+//  Created by Cristina Cabral on 2025-02-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOSApp4/iOSApp4App.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp4App.swift
+//  iOSApp4
+//
+//  Created by Cristina Cabral on 2025-02-07.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
